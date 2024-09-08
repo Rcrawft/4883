95301be2d191ecea3027cd08f59d9bea08287480
Rcrawft <crawfordryan275@gmail.com>
2024-09-08 11:40:08 -0500

    Update main.cpp to use cin/cout

--- Assignments/10783/main.cpp
+++ Assignments/10783/main.cpp
@@ -1,30 +1,22 @@
-#include <fstream>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  ofstream outfile;
-  ifstream infile;
   int first, second, total, sum;
-  infile.open("infile.txt");
-  outfile.open("outfile.txt");
 
-  infile >> total;
+  cin >> total;
 
-  for (int i = 1; i <= total; i++) 
-  {
+  for (int i = 1; i <= total; i++) {
     sum = 0;
-    infile >> first >> second;
-    if(first % 2 == 0)
-      first ++;
-    for (int j = first; j <= second; j+=2) 
-    {
+    .cin >> first >> second;
+    if (first % 2 == 0)
+      first++;
+    for (int j = first; j <= second; j += 2) {
       sum += j;
     }
-    outfile << "Case " << i << ": " << sum << endl;
+    cout << "Case " << i << ": " << sum << endl;
   }
-
 
   return 0;
 }
