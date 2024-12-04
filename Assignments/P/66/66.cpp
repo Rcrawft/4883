46f82d3ea5265bda44d4169c1d57ae047ba1f5e6
Rcrawft <crawfordryan275@gmail.com>
2024-12-04 18:40:46 +0000

    Create 66.cpp

---
+++ Assignments/P/66/66.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size() - 1; i >= 0; i--)
+        {
+            if(digits[i] == 9)
+            {
+                digits[i] = 0;
+            }
+            else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        vector<int> v(digits.size() + 1, 0);
+        v[0] = 1;
+        return v;
+    }
+};
